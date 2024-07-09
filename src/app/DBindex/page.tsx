e0dc0975742eae818490caafcd44e2c5d8629317
amishapai <amishapaib@gmail.com>
2024-07-09 13:35:39 +0530

    .

---
+++ src/app/DBindex/page.tsx
@@ -0,0 +1,22 @@
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import MainContent from '../components/MainContent';
+import Head from 'next/head';
+import React from 'react';
+
+const Home: React.FC = () => {
+    return (
+        <>
+            <Head>
+                <title>Car Dashboard</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <link href="https://fonts.googleapis.com/css2?family=Urbanist:wght@400&display=swap" rel="stylesheet" />
+            </Head>
+            <div className="container">
+                <Header />
+                <MainContent />
+                <Footer />
+            </div>
+        </>
+    );
+}
