4cbf59c68380ddb9a0221f081dc681c8ec93c969
Ayush <ayushratan082@gmail.com>
2024-07-22 23:38:09 +0530

    fix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,9 +8,7 @@ const Footer: React.FC = () => {
     return (
         <footer className="footer hidden fade-in-footer">
             <div className="toolbar">
-                <button className="icon-button" style={{ backgroundImage: "url('/images/Group 4548.svg')" }} onClick={() => openNewPage('https://example.com/page1')}></button>
-                <button className="icon-button" style={{ backgroundImage: "url('/images/Vector (3).svg')" }} onClick={() => openNewPage('https://example.com/page2')}></button>
-                <button className="icon-button" style={{ backgroundImage: "url('/images/Group 427318962.svg')" }} onClick={() => openNewPage('https://example.com/page6')}></button>
+               
           
             </div>
         </footer>
